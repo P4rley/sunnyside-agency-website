1922e054207c224747846aa85cb49661c30d18ee
P4rley <adwihersa@gmail.com>
2021-12-14 15:22:55 +0700

    first commit

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -10,7 +10,7 @@ const Testimonial = () => {
       <h1 className="sunnyside__testimonial-heading">Client Testimonial</h1>
       <div className="sunnyside__testimonial-container">
         <div className="sunnyside__testimonial-content">
-          <img src={imageOne} alt="picture" />
+          <img src={imageOne} alt="testimonialpic" />
           <p>
             We put our trust in Sunnyside wand they delivered, making sure our
             needs were met and deadlines were always hit
@@ -19,7 +19,7 @@ const Testimonial = () => {
           <span>Marketing Director</span>
         </div>
         <div className="sunnyside__testimonial-content">
-          <img src={imageThree} alt="picture" />
+          <img src={imageThree} alt="testimonialpic" />
           <p>
             Sunnyside's enthusiasm coupled with their keen interest in our
             brand's success made it a satisfying and enjoyable experience.
@@ -28,7 +28,7 @@ const Testimonial = () => {
           <span>Chief Operating Oofficer</span>
         </div>
         <div className="sunnyside__testimonial-content">
-          <img src={imageTwo} alt="" />
+          <img src={imageTwo} alt="testimonialpic" />
           <p>
             Incredible end result! Our sales increased over 400% when we worked
             with Sunnyside. Highly recommended!
